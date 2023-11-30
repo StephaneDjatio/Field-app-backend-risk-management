ca4b654c34a014976df7106ee71ee1da6a5e892a
DJATIO TCHOTEZO Stéphane <stephanedjatio@gmail.com>
2023-11-30 09:13:32 +0100

    Update urls.py

--- webRisk/urls.py
+++ webRisk/urls.py
@@ -29,13 +29,13 @@ urlpatterns = [
 ]
 
 # Serving the media files in development mode
-# if settings.DEBUG:
-#     urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
-# else:
-#     urlpatterns += staticfiles_urlpatterns()
+if settings.DEBUG:
+    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+else:
+    urlpatterns += staticfiles_urlpatterns()
 
-urlpatterns += [
-    re_path(r'^media/(?P<path>.*)$', serve, {
-        'document_root': settings.MEDIA_ROOT,
-    }),
-]
+# urlpatterns += [
+#     re_path(r'^media/(?P<path>.*)$', serve, {
+#         'document_root': settings.MEDIA_ROOT,
+#     }),
+# ]
